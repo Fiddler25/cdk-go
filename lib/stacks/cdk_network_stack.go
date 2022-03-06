11d961b3a3a5d31ec89e3392a08187fdfbdfc53e
AtsushiKanayama <rager.mka.ti@icloud.com>
2022-03-06 18:49:32 +0900

    network_stack作成

---
+++ lib/stacks/cdk_network_stack.go
@@ -0,0 +1,82 @@
+package stacks
+
+import (
+	cdk "github.com/aws/aws-cdk-go/awscdk/v2"
+	ec2 "github.com/aws/aws-cdk-go/awscdk/v2/awsec2"
+	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/aws/jsii-runtime-go"
+)
+
+func CdkNetworkStack(scope constructs.Construct, id string, props *cdk.StackProps) (ec2.CfnVPC, ec2.CfnSubnet) {
+	var sprops cdk.StackProps
+	if props != nil {
+		sprops = *props
+	}
+	stack := cdk.NewStack(scope, &id, &sprops)
+
+	// Vpc
+	vpc := ec2.NewCfnVPC(stack, jsii.String("Vpc"), &ec2.CfnVPCProps{
+		CidrBlock: jsii.String("10.0.0.0/21"),
+		Tags:      &[]*cdk.CfnTag{{Key: jsii.String("Name"), Value: jsii.String("MyVPC")}},
+	})
+
+	// PublicSubnet
+	publicSubnet1 := ec2.NewCfnSubnet(stack, jsii.String("PublicSubnet1"), &ec2.CfnSubnetProps{
+		AvailabilityZone:    jsii.String("ap-northeast-1a"),
+		CidrBlock:           jsii.String("10.0.0.0/24"),
+		VpcId:               vpc.Ref(),
+		MapPublicIpOnLaunch: true,
+		Tags:                &[]*cdk.CfnTag{{Key: jsii.String("Name"), Value: jsii.String("PublicSubnet1")}},
+	})
+
+	ec2.NewCfnSubnet(stack, jsii.String("PublicSubnet2"), &ec2.CfnSubnetProps{
+		AvailabilityZone:    jsii.String("ap-northeast-1c"),
+		CidrBlock:           jsii.String("10.0.1.0/24"),
+		VpcId:               vpc.Ref(),
+		MapPublicIpOnLaunch: true,
+		Tags:                &[]*cdk.CfnTag{{Key: jsii.String("Name"), Value: jsii.String("PublicSubnet2")}},
+	})
+
+	// PrivateSubnet
+	ec2.NewCfnSubnet(stack, jsii.String("PrivateSubnet1"), &ec2.CfnSubnetProps{
+		AvailabilityZone: jsii.String("ap-northeast-1a"),
+		CidrBlock:        jsii.String("10.0.2.0/24"),
+		VpcId:            vpc.Ref(),
+		Tags:             &[]*cdk.CfnTag{{Key: jsii.String("Name"), Value: jsii.String("PrivateSubnet1")}},
+	})
+
+	ec2.NewCfnSubnet(stack, jsii.String("PrivateSubnet2"), &ec2.CfnSubnetProps{
+		AvailabilityZone: jsii.String("ap-northeast-1c"),
+		CidrBlock:        jsii.String("10.0.3.0/24"),
+		VpcId:            vpc.Ref(),
+		Tags:             &[]*cdk.CfnTag{{Key: jsii.String("Name"), Value: jsii.String("PrivateSubnet2")}},
+	})
+
+	// InternetGateway
+	igw := ec2.NewCfnInternetGateway(stack, jsii.String("InternetGateway"), &ec2.CfnInternetGatewayProps{
+		Tags: &[]*cdk.CfnTag{{Key: jsii.String("Name"), Value: jsii.String("MyInternetGateway")}},
+	})
+
+	ec2.NewCfnVPCGatewayAttachment(stack, jsii.String("VPCGatewayAttachment"), &ec2.CfnVPCGatewayAttachmentProps{
+		VpcId:             vpc.Ref(),
+		InternetGatewayId: igw.Ref(),
+	})
+
+	// RouteTable
+	routeTable := ec2.NewCfnRouteTable(stack, jsii.String("RouteTable"), &ec2.CfnRouteTableProps{
+		VpcId: vpc.Ref(),
+	})
+
+	ec2.NewCfnRoute(stack, jsii.String("Route"), &ec2.CfnRouteProps{
+		RouteTableId:         routeTable.Ref(),
+		DestinationCidrBlock: jsii.String("0.0.0.0/0"),
+		GatewayId:            igw.Ref(),
+	})
+
+	ec2.NewCfnSubnetRouteTableAssociation(stack, jsii.String("RouteTableAssociation1"), &ec2.CfnSubnetRouteTableAssociationProps{
+		RouteTableId: routeTable.Ref(),
+		SubnetId:     publicSubnet1.Ref(),
+	})
+
+	return vpc, publicSubnet1
+}
--- main.go
+++ main.go
@@ -1,30 +1,14 @@
 package main
 
 import (
+	"github.com/Fiddler25/cdk-go/lib/stacks"
 	cdk "github.com/aws/aws-cdk-go/awscdk/v2"
-	"github.com/aws/aws-cdk-go/awscdk/v2/awssqs"
-	"github.com/aws/constructs-go/constructs/v10"
-	"github.com/aws/jsii-runtime-go"
 )
-
-func NewCdkGoStack(scope constructs.Construct, id string, props *cdk.StackProps) cdk.Stack {
-	var sprops cdk.StackProps
-	if props != nil {
-		sprops = *props
-	}
-	stack := cdk.NewStack(scope, &id, &sprops)
-
-	awssqs.NewQueue(stack, jsii.String("CdkGoQueue"), &awssqs.QueueProps{
-		VisibilityTimeout: cdk.Duration_Seconds(jsii.Number(300)),
-	})
-
-	return stack
-}
 
 func main() {
 	app := cdk.NewApp(nil)
 
-	NewCdkGoStack(app, "CdkGoStack", &cdk.StackProps{})
+	vpc, publicSubnet1 := stacks.CdkNetworkStack(app, "CdkNetworkStack", &cdk.StackProps{})
 
 	app.Synth(nil)
 }
